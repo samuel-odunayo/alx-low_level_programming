8d6bba9e2cf5ac67d49b44b601403e41e34f73f3
odunayo <odunakomolede@gmail.com>
2022-09-09 05:34:25 -0700

    put Script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
 
 int main(void)
-
-{ puts("Programming is like building a multilingual puzzle\n");
-       	return (0); }
+{ puts("Programming is like building a multilingual puzzle\n"); return (0); }
