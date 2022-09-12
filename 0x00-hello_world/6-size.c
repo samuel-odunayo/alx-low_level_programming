33f12c11866f341587a6c3f33fc51fa1142825c7
odunayo <odunakomolede@gmail.com>
2022-09-12 05:04:26 -0700

    task size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main(void) { int a; long int b; long long int c; char d; float f;
+int main(void) {
+       
+	int a;
+	
+	long int b;
+       
+	long long int c;
+       
+	char d;
+       
+	float f;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
@@ -11,4 +22,5 @@ int main(void) { int a; long int b; long long int c; char d; float f;
 
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-	return (0); }
+	return (0);
+}
