32fa1535ceeacee26dabbca23a75d6a97d295007
odunayo <odunakomolede@gmail.com>
2022-09-13 19:55:58 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main -  prints _putchar, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(viod)
+{
+	char task[9] = "_putchar";
+	int i;
+
+	i=0;
+	while (i<8)
+	{
+		_putchar(task[i]);
+		i++;
+	}
+
+	_putchar('\n);
+	return(0);
+}
