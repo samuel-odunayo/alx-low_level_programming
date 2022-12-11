d1eb5b37d0a648c9c3078d39602414fb556015dc
Odunayo Akomolede <odunakomolede@gmail.com>
2022-12-10 19:24:45 -0800

    C-variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return Always 0 (success)
+ */
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; <= 'z'; c++)
+		putchar(C);
+		putchar('\n');
+
+	return (0);
+}
