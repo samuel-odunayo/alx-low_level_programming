e17f853d891df6230db669f966fd7d688dad955c
Odunayo Akomolede <odunakomolede@gmail.com>
2022-12-13 16:07:01 -0800

    low-level_language

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ *print_alphabet - Print the alphabet in lower case.
+ *
+ *Return: void.
+ */
+void print_alphabet(void)
+{
+
+	int i;
+
+	for (i = 97; i < 123; ++)
+		{
+		_putchar(i);
+		}
+		_putchar('n\');
+}
