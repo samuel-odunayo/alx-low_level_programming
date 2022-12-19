27b94e1c1d305f084f819ed437fdbbd5d97ca121
Odunayo Akomolede <odunakomolede@gmail.com>
2022-12-19 14:20:04 -0800

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,10 @@
 #include <main.h>
-#include <stdio>
-
+/**
+ * reset_to_98 -a function that takes a pointer to an int
+ * @n: puntero
+ * Return: (0)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
