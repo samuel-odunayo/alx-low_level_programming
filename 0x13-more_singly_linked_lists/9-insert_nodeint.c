abb6daa79fc827723c5c245d7ed993953c9f272b
Odunayo Akomolede <odunakomolede@gmail.com>
2023-01-31 20:53:54 -0800

    More linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
 	listint_t *current, *new;
-	
+
 	current = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
